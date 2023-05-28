eda135e5058fc93a5c8b3b1507cf6a50435c2fef
The-Pushkin <cosmin.petolea@gmail.com>
2023-05-28 23:41:58 +0300

    Reformatted all files

--- app/src/main/java/com/example/eateractive_client/server/models/MenuItemModel.kt
+++ app/src/main/java/com/example/eateractive_client/server/models/MenuItemModel.kt
@@ -2,7 +2,7 @@ package com.example.eateractive_client.server.models
 
 import com.google.gson.annotations.SerializedName
 
-data class MenuItemModel (
+data class MenuItemModel(
     @SerializedName("item_id")
     val id: Int,
     val name: String,
--- app/src/main/java/com/example/eateractive_client/server/models/TestModel.kt
+++ app/src/main/java/com/example/eateractive_client/server/models/TestModel.kt
@@ -2,7 +2,7 @@ package com.example.eateractive_client.server.models
 
 import com.google.gson.annotations.SerializedName
 
-data class TestModel (
+data class TestModel(
     @SerializedName("Ia cu paine")
     val text: String
 )
